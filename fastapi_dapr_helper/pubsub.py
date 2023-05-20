b37d6241bbbe53d30d8fccd549472ee67e149f98
Clayton Black <CBlack@fanatics.com>
2023-05-20 00:14:41 -0400

    Refactor pubsub subscription decorator
    This commit refactors the `subscribe` function in `pubsub.py` to accept a list of tags instead of a string, and adds type checking for input arguments. The function now returns a callable decorator with an updated docstring that includes information about expected inputs.

--- fastapi_dapr_helper/pubsub.py
+++ fastapi_dapr_helper/pubsub.py
@@ -1,20 +1,39 @@
-from typing import Optional, Dict, Any
+from typing import Optional, Dict, Any, List, Union
 
 from fastapi import FastAPI, APIRouter
 
 
 def subscribe(
-    app: [FastAPI, APIRouter],
+    app: Union[FastAPI, APIRouter],
     path: str,
     pubsub: str,
     topic: str,
-    tags: Optional[str] = None,
+    tags: Optional[List[str]] = None,
     metadata: Optional[Dict[str, str]] = None,
     dead_letter_topic: Optional[str] = None,
     openapi_extra: Optional[Dict[str, Any]] = None,
     *args,
     **kwargs,
-):
+) -> callable:
+    """
+    A decorator used in combination with a route generator to create subscriptions to Dapr pubsub topics with FastAPI.
+
+    # TODO: Add example
+    # TODO: update tests for invalid types of inputs.
+
+    Args:
+        app (Union[FastAPI, APIRouter]): The FastAPI or APIRouter application.
+        path (str): The endpoint path.
+        pubsub (str): The pubsub name.
+        topic (str): The topic name.
+        tags (Optional[List[str]]): Optional tags for the endpoint.
+        metadata (Optional[Dict[str, str]]): Optional metadata.
+        dead_letter_topic (Optional[str]): Optional dead letter topic.
+        openapi_extra (Optional[Dict[str, Any]]): Optional extra OpenAPI info.
+
+    Returns:
+        callable: The decorator.
+    """
     if metadata is None:
         metadata = {}
 
@@ -29,6 +48,10 @@ def subscribe(
         raise TypeError('Expected string for "pubsub"')
     if not isinstance(topic, str):
         raise TypeError('Expected string for "topic"')
+    if tags is not None and not isinstance(tags, list):
+        raise TypeError('Expected list for "tags"')
+    if dead_letter_topic is not None and not isinstance(dead_letter_topic, str):
+        raise TypeError('Expected string for "dead_letter_topic"')
 
     def decorator(func):
         openapi_extra["dapr"] = {
